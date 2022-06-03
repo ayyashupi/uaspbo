3c637254eafd138d853b35e7e132022c46386f25
SanjayaWisnu33 <61383222+SanjayaWisnu33@users.noreply.github.com>
2022-06-03 19:15:19 +0700

    Objek
    Objek Java

---
+++ uaspbo/src/object/mobil.java
@@ -0,0 +1,149 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package object;
+
+/**
+ *
+ * @author User
+ */
+public class mobil {
+    private int id_mobil;
+    private String nopol;
+    private String merk;
+    private String jenis;
+    private int jml_penumpang;
+    private String transmisi; 
+    private String BBM; //jenis BBM
+    private int harga_sewa;
+    private String status;
+
+    /**
+     * @return the id_mobil
+     */
+    public int getId_mobil() {
+        return id_mobil;
+    }
+
+    /**
+     * @param id_mobil the id_mobil to set
+     */
+    public void setId_mobil(int id_mobil) {
+        this.id_mobil = id_mobil;
+    }
+
+    /**
+     * @return the nopol
+     */
+    public String getNopol() {
+        return nopol;
+    }
+
+    /**
+     * @param nopol the nopol to set
+     */
+    public void setNopol(String nopol) {
+        this.nopol = nopol;
+    }
+
+    /**
+     * @return the merk
+     */
+    public String getMerk() {
+        return merk;
+    }
+
+    /**
+     * @param merk the merk to set
+     */
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    /**
+     * @return the jenis
+     */
+    public String getJenis() {
+        return jenis;
+    }
+
+    /**
+     * @param jenis the jenis to set
+     */
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    /**
+     * @return the jml_penumpang
+     */
+    public int getJml_penumpang() {
+        return jml_penumpang;
+    }
+
+    /**
+     * @param jml_penumpang the jml_penumpang to set
+     */
+    public void setJml_penumpang(int jml_penumpang) {
+        this.jml_penumpang = jml_penumpang;
+    }
+
+    /**
+     * @return the transmisi
+     */
+    public String getTransmisi() {
+        return transmisi;
+    }
+
+    /**
+     * @param transmisi the transmisi to set
+     */
+    public void setTransmisi(String transmisi) {
+        this.transmisi = transmisi;
+    }
+
+    /**
+     * @return the BBM
+     */
+    public String getBBM() {
+        return BBM;
+    }
+
+    /**
+     * @param BBM the BBM to set
+     */
+    public void setBBM(String BBM) {
+        this.BBM = BBM;
+    }
+
+    /**
+     * @return the harga_sewa
+     */
+    public int getHarga_sewa() {
+        return harga_sewa;
+    }
+
+    /**
+     * @param harga_sewa the harga_sewa to set
+     */
+    public void setHarga_sewa(int harga_sewa) {
+        this.harga_sewa = harga_sewa;
+    }
+
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+}
