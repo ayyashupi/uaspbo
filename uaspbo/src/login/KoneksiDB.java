862487ce0ec56e8d3fe799313a3d19889dd70c9d
ayyashupi <ayyash@upi.edu>
2022-06-05 17:13:02 +0700

    login, register, profile, menu

--- uaspbo/src/login/KoneksiDB.java
+++ uaspbo/src/login/KoneksiDB.java
@@ -1,8 +1,6 @@
 package login;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import javax.swing.JOptionPane;
 
 public class KoneksiDB {
