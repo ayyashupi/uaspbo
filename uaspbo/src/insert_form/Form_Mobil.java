8b88b52e350e634e2cff2f39715edef20ff73b1b
RakaRyG <rakaguntara10@gmail.com>
2022-06-05 23:10:36 +0700

    klo berubah revert aja

--- uaspbo/src/insert_form/Form_Mobil.java
+++ uaspbo/src/insert_form/Form_Mobil.java
@@ -413,6 +413,10 @@ public class Form_Mobil extends javax.swing.JFrame {
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
