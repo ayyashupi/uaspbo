3abef9fcce34018cd5fa22ef7b959ab51241d019
ayyashupi <ayyash@upi.edu>
2022-06-05 18:04:34 +0700

    kapitalisasi

--- uaspbo/src/insert_form/form_penyewa.java
+++ uaspbo/src/insert_form/form_penyewa.java
@@ -266,6 +266,22 @@ public class Form_Penyewa extends javax.swing.JFrame {
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
--- uaspbo/src/insert_form/form_supir.java
+++ uaspbo/src/insert_form/form_supir.java
@@ -213,6 +213,14 @@ public class Form_Supir extends javax.swing.JFrame {
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
